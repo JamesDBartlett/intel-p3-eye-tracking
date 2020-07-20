98216a9e9f8db6f51bdf4e5ae55dcc2891eb7e6a
James Bartlett <jimmyb@gmail.com>
2020-07-20 17:48:52 -0500

    removed examples from main repo

--- p3-examples/bhadreshpsavani/run.sh
+++
@@ -1,5 +0,0 @@
-python3 src/main.py -fd intel/face-detection-adas-binary-0001/FP32-INT1/face-detection-adas-binary-0001.xml \
-    -lr intel/landmarks-regression-retail-0009/FP16-INT8/landmarks-regression-retail-0009.xml \
-    -hp intel/head-pose-estimation-adas-0001/FP16-INT8/head-pose-estimation-adas-0001.xml \
-    -ge intel/gaze-estimation-adas-0002/FP16-INT8/gaze-estimation-adas-0002.xml \
-    -i bin/demo.mp4 -flags ff fl fh fg
