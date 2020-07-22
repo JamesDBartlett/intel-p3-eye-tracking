6509ba84942bc50f650eb4db6891ce95e663b1d6
James Bartlett <jimmyb@gmail.com>
2020-07-21 20:13:18 -0500

    renamed run.sh to demo.sh

--- demo.sh
+++ demo.sh
@@ -1,11 +1,11 @@
-
+# Run this script to execute the demo
 
 python3 src/main.py \
         -fd models/intel/face-detection-adas-0001/FP16-INT8/face-detection-adas-0001.xml \
         -fld models/intel/landmarks-regression-retail-0009/FP16/landmarks-regression-retail-0009.xml \
         -ge models/intel/gaze-estimation-adas-0002/FP16/gaze-estimation-adas-0002.xml \
         -hpe models/intel/head-pose-estimation-adas-0001/FP16/head-pose-estimation-adas-0001.xml \
-        -i bin/demo.mp4 \
+        -i media/demo.mp4 \
         -d HETERO:MYRIAD,CPU \
         --print 
         --no_move
