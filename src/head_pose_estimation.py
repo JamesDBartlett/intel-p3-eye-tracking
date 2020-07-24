b3afcf0d624b52ff6aba772605647e6545646f3b
James Bartlett <jimmyb@gmail.com>
2020-07-24 17:49:11 -0500

    imports & method comments

--- src/head_pose_estimation.py
+++ src/head_pose_estimation.py
@@ -2,31 +2,32 @@
     head_pose_estimation.py
     Author: @JamesDBartlett3
 """
+import cv2
+import time
+import numpy as np
+from inference import Network
 
 
 class HeadPoseEstimation:
     """
-    Class for the Face Detection Model.
+        Head Pose Estimation Class
     """
 
     def __init__(self, model_name, device="CPU", extensions = None):
         """
-        TODO: Use this to set your instance variables.
+            set instance variables
         """
         raise NotImplementedError
 
     def load_model(self):
         """
-        TODO: You will need to complete this method.
-        This method is for loading the model to the device specified by the user.
-        If your model requires any Plugins, this is where you can load them.
+            loading the model specified by the user to the device
         """
         raise NotImplementedError
 
     def predict(self, image):
         """
-        TODO: You will need to complete this method.
-        This method is meant for running predictions on the input image.
+            run predictions on the input image
         """
         raise NotImplementedError
 
@@ -35,14 +36,12 @@ class HeadPoseEstimation:
 
     def preprocess_input(self, image):
         """
-        Before feeding the data into the model for inference,
-        you might have to preprocess it. This function is where you can do that.
+            preprocess input image
         """
         raise NotImplementedError
 
     def preprocess_output(self, outputs):
         """
-        Before feeding the output of this model to the next model,
-        you might have to preprocess the output. This function is where you can do that.
+            preprocess output image
         """
         raise NotImplementedError
