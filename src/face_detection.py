ede30b87fbcad70afd911a91566d87311c6329ba
James Bartlett <jimmyb@gmail.com>
2020-07-23 21:23:31 -0500

    completed face_detection.py

--- src/face_detection.py
+++ src/face_detection.py
@@ -13,40 +13,61 @@ class FaceDetection:
     Face Detection Class.
     """
 
+    def now():
+        return time.time()
+
     def __init__(self, model_name, device="CPU", extensions=None):
         """
-        TODO: Use this to set your instance variables.
+            set instance variables
         """
-        raise NotImplementedError
+        self.model_xml = model_name
+        self.device = device
+        self.extensions = extensions
+        self.infer_network = Network()
 
     def load_model(self):
         """
-        TODO: You will need to complete this method.
-        This method is for loading the model to the device specified by the user.
-        If your model requires any Plugins, this is where you can load them.
+            loading the model specified by the user to the device
         """
-        raise NotImplementedError
+        self.infer_network.load_model(self.model_xml, self.device, self.extensions)
 
     def predict(self, image):
         """
-        TODO: You will need to complete this method.
-        This method is meant for running predictions on the input image.
+            run predictions on the input image
         """
-        raise NotImplementedError
+        self.infer_network.exec_net(image)
+        if self.infer_network.wait() == 0:
+            return self.infer_network.get_output()[self.infer_network.out_blob]
 
     def check_model(self):
         raise NotImplementedError
 
     def preprocess_input(self, image):
         """
-        Before feeding the data into the model for inference,
-        you might have to preprocess it. This function is where you can do that.
+            preprocess input image
         """
-        raise NotImplementedError
+        input_shape = self.infer_network.get_input_shape()
+        frame = np.copy(image)
+        frame = cv2.resize(frame, (input_shape[3], input_shape[2]))
+        frame = frame.transpose((2, 0, 1))
+        frame = frame.reshape(1, *frame.shape)
+        return frame
 
-    def preprocess_output(self, outputs):
+    def preprocess_output(self, outputs, img, overlay_inference, probability_threshold):
         """
-        Before feeding the output of this model to the next model,
-        you might have to preprocess the output. This function is where you can do that.
+            preprocess output image
         """
-        raise NotImplementedError
+        h, w = img.shape[0:2]
+        boxes = []
+
+        for i in range(len(outputs[0][0])):
+            box = outputs[0][0][i]
+            confidence = box[2]
+            if not confidence <= probability_threshold:
+                b = [xmin = int(w * box[3]), ymin = int(h * box[4]),
+                    xmax = int(w * box[5]), ymax = int(h * box[6])]
+
+                if(overlay_inference):
+                    cv2.rectangle(img, (b[0], b[1]), (b[2], b[3]), (0, 255, 0), 1)
+                boxes.append([b[0], b[1], b[2], b[3]])
+        return img, boxes
