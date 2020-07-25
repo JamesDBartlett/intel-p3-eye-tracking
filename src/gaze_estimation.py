283dd9c6f0ad89aacc40f1e78e85ef7cd47d5fcb
James Bartlett <jimmyb@gmail.com>
2020-07-25 09:26:59 -0500

    gaze_estimation complete

--- src/gaze_estimation.py
+++ src/gaze_estimation.py
@@ -13,15 +13,26 @@ def print_coords(img, axis_name, axis_value, color):
     cv2.putText(
         img,
         axis_name + str("{:.1f}".format(axis_value * axes_misc[axis_name][1])),
-        (15, axes_misc[axis_name][0]),
+        (20, axes_misc[list(axes_misc.keys())[0]][1]),
         0,
+        0.6,
         color,
         1,
     )
 
 
+def arrowed_line(img, x, y, qx, qy):
+    cv2.arrowedLine(
+        img,
+        (qx, qy),
+        (qx + int(x * 100), qy + int(-100 * y)),
+        list(color.values())[2],
+        5,
+    )
+
+
 def xy_min_max(p, d, m, f):
-    return int(p + (d * m) // 2) if int(p + (d * m) // 2) >= f else 0
+    return int(p + (d * m) // 2) if int(p + (d * m) // 2) >= f else f
 
 
 class GazeEstimation:
@@ -44,13 +55,13 @@ class GazeEstimation:
         """
         self.infer_network.load_model(self.model_xml, self.device, self.extensions)
 
-    def predict(self, image):
+    def predict(self, head_pose, l_image, r_image):
         """
             run predictions on the input image
         """
-        self.infer_network.exec_net(image)
+        self.infer_network.exec_net(head_pose, l_image, r_image)
         if self.infer_network.wait() == 0:
-            return self.infer_network.get_output()[self.infer_network.out_blob]
+            return self.infer_network.get_output()[self.infer_network.output_blob]
 
     def check_model(self):
         raise NotImplementedError
@@ -59,14 +70,14 @@ class GazeEstimation:
         """
             preprocess input image
         """
-        l_shape = r_shape = [1, 3, 50, 50]
+        l_shape = r_shape = [1, 3, 60, 60]
         l_image, r_image, l_frame, r_frame = [None] * 4
-        L, R = (
+        eyes = (
             [l_coords, l_shape, l_image, 20, l_frame],
             [r_coords, r_shape, r_image, 100, r_frame],
         )
         p_frames = []
-        for i in (L, R):
+        for i in eyes:
             x = i[0][0]
             y = i[0][1]
             h = i[1][2]
@@ -75,30 +86,25 @@ class GazeEstimation:
             xmax = xy_min_max(x, w, 1, face.shape[1])
             ymin = xy_min_max(y, h, -1, 0)
             ymax = xy_min_max(y, h, 1, face.shape[0])
-            i[2] = face[ymin:ymax, xmin:xmax]
+            p_image = face[ymin:ymax, xmin:xmax]
             if overlay_inference:
-                frame[150 : 150 + i[2].shape[0], i[3] : i[3] + i[2].shape[1]] = i[2]
-            i[4] = cv2.resize(i[2], (w, h))
-            i[4] = i[4].transpose((2, 0, 1))
-            i[4] = i[4].reshape(1, i[4].shape)
-            p_frames.append(i[4])
+                frame[
+                    150 : 150 + p_image.shape[0], i[3] : i[3] + p_image.shape[1]
+                ] = p_image
+            p_frame = cv2.resize(p_image, (w, h))
+            p_frame = p_frame.transpose((2, 0, 1))
+            p_frame = p_frame.reshape(1, *p_frame.shape)
+            p_frames.append(p_frame)
         return frame, p_frames[0], p_frames[1]
 
     def preprocess_output(
-        self,
-        outputs,
-        img,
-        box,
-        l_coords,
-        r_coords,
-        overlay_inference,
+        self, outputs, img, box, l_coords, r_coords, overlay_inference,
     ):
         """
             preprocess output image
         """
         x, y, z = [outputs[0][i] for i in range(len(outputs[0]))]
         xmin, ymin, _, _, = box
-
         if overlay_inference:
             lc, rc = (l_coords[0:2], r_coords[0:2])
             lx = int(xmin + lc[0])
@@ -108,21 +114,7 @@ class GazeEstimation:
             for axis in [x, y, z]:
                 for j in axes_misc.keys():
                     for k in color.keys():
-                        print_coords(
-                            img, axes_misc[j], axis, color[k]
-                        )
-                cv2.arrowedLine(
-                    img,
-                    (lx, ly),
-                    (lx + int(x * 100), ly + int(-100 * y)),
-                    color.values[2],
-                    5,
-                )
-                cv2.arrowedLine(
-                    img,
-                    (rx, ry),
-                    (rx + int(x * 100), ry + int(-100 * y)),
-                    color.values[2],
-                    5,
-                )
+                        print_coords(img, j, axis, color[k])
+                arrowed_line(img, x, y, lx, ly)
+                arrowed_line(img, x, y, rx, ry)
         return img, [x, y, z]
