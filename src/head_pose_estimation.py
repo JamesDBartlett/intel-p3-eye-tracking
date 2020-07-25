a9da916a1aec815843504f29f2bf02593942b304
James Bartlett <jimmyb@gmail.com>
2020-07-25 02:09:17 -0500

    populated all but one method

--- src/head_pose_estimation.py
+++ src/head_pose_estimation.py
@@ -4,6 +4,7 @@
 """
 import cv2
 import time
+import math
 import numpy as np
 from inference import Network
 
@@ -17,19 +18,24 @@ class HeadPoseEstimation:
         """
             set instance variables
         """
-        raise NotImplementedError
+        self.model_xml = model_name
+        self.device = device
+        self.extensions = extensions
+        self.infer_network = Network()
 
     def load_model(self):
         """
             loading the model specified by the user to the device
         """
-        raise NotImplementedError
+        self.infer_network.load_model(self.model_xml, self.device, self.extensions)
 
     def predict(self, image):
         """
             run predictions on the input image
         """
-        raise NotImplementedError
+        self.infer_network.exec_net(image)
+        if self.infer_network.wait() == 0:
+            return self.infer_network.get_output()[self.infer_network.out_blob]
 
     def check_model(self):
         raise NotImplementedError
@@ -38,7 +44,12 @@ class HeadPoseEstimation:
         """
             preprocess input image
         """
-        raise NotImplementedError
+        input_shape = self.infer_network.get_input_shape()
+        frame = np.copy(image)
+        frame = cv2.resize(frame, (input_shape[3], input_shape[2]))
+        frame = frame.transpose((2, 0, 1))
+        frame = frame.reshape(1, *frame.shape)
+        return frame
 
     def preprocess_output(self, outputs):
         """
