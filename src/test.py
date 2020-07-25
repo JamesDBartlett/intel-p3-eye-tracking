474873b2db1f5754c37f304e8a37904c84a8d7c2
James Bartlett <jimmyb@gmail.com>
2020-07-25 02:01:38 -0500

    fun ideas

--- src/test.py
+++ src/test.py
@@ -8,3 +8,23 @@ for i in [x, y, z]:
     for j in axes_misc.keys():
         for k in color.keys():
             print(i, j, k, color[k], axes_misc[j])
+
+def xy_min_max(p, d, m, f):
+    return(int(p + (d * m) // 2) if int(p + (d * m) // 2) >= f else 0)
+
+f_shape = [300, 500]
+
+l_coords = [100, 150]
+r_coords = [200, 250]
+l_shape = r_shape = [1, 3, 50, 50]
+L, R = ([l_coords, l_shape], [r_coords, r_shape])
+for i in (L, R):
+    x = i[0][0]
+    y = i[0][1]
+    h = i[1][2]
+    w = i[1][3]
+    xmin = xy_min_max(x, w, -1, 0)
+    xmax = xy_min_max(x, w, 1, f_shape[1])
+    ymin = xy_min_max(y, h, -1, 0)
+    ymax = xy_min_max(y, h, 1, f_shape[0])
+    print(xmin, xmax, ymin, ymax)
