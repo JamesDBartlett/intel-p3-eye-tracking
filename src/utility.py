909b199bf3e138ecc593dc6b3e23b1c68bd37d6b
James Bartlett <jimmyb@gmail.com>
2020-07-25 14:17:55 -0500

    added itertools

--- src/utility.py
+++ src/utility.py
@@ -1,2 +1,7 @@
+from itertools import islice
+
+def take(n, iterable):
+    return list(islice(iterable, n))
+
 color = {"R": (255, 0, 0), "G": (0, 255, 0), "B": (0, 0, 255)}
 axes_misc = {"X": [15, 100], "Y": [30, 100], "Z": [45, 1]}
