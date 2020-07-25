e26a1b9d34b9bfcf56bec186f00021013307d964
James Bartlett <jimmyb@gmail.com>
2020-07-25 00:29:48 -0500

    abstracted coordinate printing to a function

--- src/gaze_estimation.py
+++ src/gaze_estimation.py
@@ -6,14 +6,25 @@ import cv2
 import time
 import numpy as np
 from inference import Network
+from utility import color, axes_misc
 
+def print_coords(img, axis_name, axis_value, color, probability_threshold):
+    cv2.putText(
+                img,
+                axis_name + str("{:.1f}".format(axis_value * axes_misc[axis_name][1])),
+                (15, axes_misc[axis_name][0]),
+                0,
+                probability_threshold,
+                color,
+                1,
+            )
 
 class GazeEstimation:
     """
         Gaze Estimation Class
     """
 
-    def __init__(self, model_name, device="CPU", extensions = None):
+    def __init__(self, model_name, device="CPU", extensions=None):
         """
             set instance variables
         """
@@ -39,15 +50,31 @@ class GazeEstimation:
     def check_model(self):
         raise NotImplementedError
 
-    def preprocess_input(self, frame, face, l_coords, r_coords, overlay_inference):
+    def preprocess_input(self, frame, box, l_coords, r_coords, overlay_inference):
         """
             preprocess input image
         """
-        l_shape = r_shape = [1,3,60,60]
-        
+        l_shape = r_shape = [1, 3, 60, 60]
 
-    def preprocess_output(self, outputs):
+    def preprocess_output(
+        self,
+        outputs,
+        img,
+        box,
+        l_coords,
+        r_coords,
+        overlay_inference,
+        probability_threshold,
+    ):
         """
             preprocess output image
         """
-        raise NotImplementedError
+        x, y, z = [outputs[0][i] for i in range(len(outputs[0]))]
+
+        if overlay_inference:
+            for axis in [x, y, z]:
+                for j in axes_misc.keys():
+                    for k in color.keys():
+                        print_coords(img, axes_misc[j], axis, color[k], probability_threshold)
+
+
