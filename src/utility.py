8e48b071f8f36ef7d2d846e0142d9e24878125f9
James Bartlett <jimmyb@gmail.com>
2020-07-25 21:51:09 -0500

    playing with different values

--- src/utility.py
+++ src/utility.py
@@ -1,7 +1,7 @@
 from itertools import islice
 
 color = {"R": (255, 0, 0), "G": (0, 255, 0), "B": (0, 0, 255), "C": (0, 255, 255), "M": (255, 0, 255), "Y": (255, 255, 0), "K": (0, 0, 0)}
-axes_misc = {"X": [15, 100], "Y": [30, 100], "Z": [45, 1]}
+axes_misc = {"X": [25, 100], "Y": [50, 100], "Z": [75, 1]}
 
 def take(n, iterable):
     return list(islice(iterable, n))
